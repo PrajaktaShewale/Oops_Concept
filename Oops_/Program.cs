fc35ff52a289445e69b309305905b268c6d31f87
PrajaktaShewale <shewalep94@gmail.com>
2022-11-12 13:28:09 +0530

    [Prajakta]Add:OopsPracticeProblems

---
+++ Oops_/Program.cs
@@ -0,0 +1,15 @@
+﻿using Oops_.InventoryManagment;
+
+namespace Oops_
+{
+    class Program
+    {
+        static string inventoryFilePath = @"D:\BridgeLabz\Oops_Concept\Oops_\Inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
+            inventoryDetailsManagement.ReadJsonFile(inventoryFilePath);
+        }
+
+    }
+}
